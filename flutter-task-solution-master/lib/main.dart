e273f25131af4275628f8dc6675c3004b87ce16a
an90ass <21670310176@ogrenci.bartin.edu.tr>
2023-12-02 23:41:45 +0300

    First-Steps-in-Flutter

---
+++ flutter-task-solution-master/lib/main.dart
@@ -0,0 +1,164 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: FacebookApp(),
+    );
+  }
+}
+
+class FacebookApp extends StatelessWidget {
+  const FacebookApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("facebook",
+              style: TextStyle(
+                  color: Colors.blueAccent,
+                  fontSize: 27,
+                  fontWeight: FontWeight.w700)),
+          centerTitle: true,
+          leading: IconButton(
+            icon: Icon(
+              Icons.menu,
+              color: Colors.blueAccent,
+              size: 30,
+            ),
+            onPressed: () {},
+          ),
+          actions: [
+            IconButton(
+                icon: Icon(
+                  Icons.message,
+                  color: Colors.blueAccent,
+                  size: 30,
+                ),
+                onPressed: () {}),
+            IconButton(
+              icon: Icon(
+                Icons.search,
+                color: Colors.blueAccent,
+                size: 30,
+              ),
+              onPressed: () {},
+            ),
+          ],
+          backgroundColor: Colors.white,
+          elevation: 20,
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                margin: EdgeInsets.only(bottom: 22),
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: Colors.blueGrey,
+                ),
+                height: 322,
+                width: double.infinity,
+                child: Text(
+                  "Anas",
+                  style: TextStyle(color: Colors.white, fontSize: 44),
+                ),
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal   ,
+                child: Row(
+                children: [
+                  Container(
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: Colors.blueGrey,
+                    ),
+                    height: 100,
+                    width: 150,
+                    child: Text(
+                      "Almaqtari",
+                      style: TextStyle(color: Colors.white, fontSize: 27),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 23),
+                
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: Colors.blueGrey,
+                    ),
+                    height: 100,
+                    width: 150,
+                    child: Text(
+                      "flutter",
+                      style: TextStyle(color: Colors.white, fontSize: 27),
+                    ),
+                  ),
+                  Container(
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: Colors.blueGrey,
+                    ),
+                    height: 100,
+                    width: 150,
+                    child: Text(
+                      "anas",
+                      style: TextStyle(color: Colors.white, fontSize: 27),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(left: 23),
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: Colors.blueGrey,
+                    ),
+                    height: 100,
+                    width: 150,
+                    child: Text(
+                      "Almaqtari",
+                      style: TextStyle(color: Colors.white, fontSize: 27),
+                    ),
+                  ),
+                
+                
+                ],
+              ),
+          
+                ),
+              
+              Container(
+                margin: EdgeInsets.only(top: 20),
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: Colors.blueGrey,
+                ),
+                height: 322,
+                width: double.infinity,
+                child: Text(
+                  "learing flutter",
+                  style: TextStyle(color: Colors.white, fontSize: 44),
+                ),
+              )
+            ],
+          ),
+        ));
+  }
+}
+// mainAxisAlignment → MainAxisAlignment.
+// crossAxisAlignment → CrossAxisAlignment.
